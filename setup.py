a95eb3528d1f73541343b33eab64bfae1ca9648a
Mike <mike@hendricks3d.com>
2020-03-24 15:39:54 -0700

    Format code with Black

--- setup.py
+++ setup.py
@@ -3,40 +3,37 @@ import os
 from setuptools import setup, find_packages
 import virtualenvconfig
 
-name = 'virtualenvconfig'
+name = "virtualenvconfig"
 dirname = os.path.dirname(os.path.abspath(__file__))
 
 # Get the long description from the README file.
-with open(os.path.join(dirname, 'README.md')) as fle:
+with open(os.path.join(dirname, "README.md")) as fle:
     long_description = fle.read()
 
 setup(
     name=name,
     version=virtualenvconfig.__version__,
-    description=r'Customize abi resolution for a given virtualenv setup.',
+    description=r"Customize abi resolution for a given virtualenv setup.",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/blurstudio/{}'.format(name),
-    download_url='https://github.com/blurstudio/{}/archive/master.zip'.format(name),
-    license='GNU LGPLv3',
+    long_description_content_type="text/markdown",
+    url="https://github.com/blurstudio/{}".format(name),
+    download_url="https://github.com/blurstudio/{}/archive/master.zip".format(name),
+    license="GNU LGPLv3",
     classifiers=[
-           'Development Status :: 3 - Alpha',
-           'Intended Audience :: Developers',
-           'Programming Language :: Python',
-           'Programming Language :: Python :: 2',
-           'Programming Language :: Python :: 2.7',
-           'Programming Language :: Python :: Implementation :: CPython',
-           'Programming Language :: Python :: Implementation :: PyPy',
-           'Operating System :: OS Independent',
-           'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
     ],
-    keywords='',
+    keywords="",
     py_modules=["virtualenvconfig"],
     include_package_data=True,
-    author='Blur Studio',
-    install_requires=[
-      'gorilla',
-      'pip>=19.1.1',
-    ],
-    author_email='pipeline@blur.com'
+    author="Blur Studio",
+    install_requires=["gorilla", "pip>=19.1.1",],
+    author_email="pipeline@blur.com",
 )
--- virtualenvconfig.py
+++ virtualenvconfig.py
@@ -4,20 +4,20 @@ import logging
 import importlib
 from distutils.sysconfig import get_python_lib
 
-__version__ = '0.0.6'
+__version__ = "0.0.6"
 
 # Configure logging, Use a special logger so this doesn't enable debug printing
 # from pip and other modules.
-logger = logging.getLogger('virtualenvconfig')
+logger = logging.getLogger("virtualenvconfig")
 # This code is likely run by sitecustomize.py, this ends up importing the module
 # twice. This check prevents doubling the text output of log messages.
 first_run = not bool(logger.handlers)
 if first_run:
-    if os.environ.get('VIRTUALENVCONFIG_VERBOSE', '').lower() == 'true':
+    if os.environ.get("VIRTUALENVCONFIG_VERBOSE", "").lower() == "true":
         console = logging.StreamHandler()
         logger.setLevel(logging.DEBUG)
         # set a format which is simpler for console use
-        formatter = logging.Formatter('-VC- %(message)s')
+        formatter = logging.Formatter("-VC- %(message)s")
         # tell the handler to use this format
         console.setFormatter(formatter)
         # add the handler to the root logger
@@ -26,8 +26,9 @@ if first_run:
 
 def is_venv():
     """ Are we running in a virtualenv https://stackoverflow.com/a/42580137 """
-    return (hasattr(sys, 'real_prefix') or
-            (hasattr(sys, 'base_prefix') and sys.base_prefix != sys.prefix))
+    return hasattr(sys, "real_prefix") or (
+        hasattr(sys, "base_prefix") and sys.base_prefix != sys.prefix
+    )
 
 
 def abi_filename():
@@ -40,7 +41,7 @@ def abi_filename():
     allows you to use the virtualenvconfig as a editable package and still
     reference the correct abi for the current virtualenv.
     """
-    return os.path.join(get_python_lib(), 'virtualenvconfig.txt')
+    return os.path.join(get_python_lib(), "virtualenvconfig.txt")
 
 
 def customize_filename():
@@ -51,27 +52,27 @@ def customize_filename():
     run when the virtualenv is activated, not if the site-packages folder is used without
     activating the virtualenv.
     """
-    return os.path.join(os.path.dirname(os.__file__), 'sitecustomize.py')
+    return os.path.join(os.path.dirname(os.__file__), "sitecustomize.py")
 
 
 def install_sitecustomize(overwrite):
     customize = customize_filename()
-    command = 'import virtualenvconfig'
+    command = "import virtualenvconfig"
     found = False
 
     if os.path.exists(customize):
         if not overwrite:
-            print('File already exists. Use --overwrite to force a update.')
+            print("File already exists. Use --overwrite to force a update.")
             print(customize)
             sys.exit(1)
-        with open(customize, 'r') as fle:
+        with open(customize, "r") as fle:
             for line in fle:
                 if command in line:
                     found = True
     if not found:
-        with open(customize, 'a') as fle:
+        with open(customize, "a") as fle:
             # Append our code to the end of the file
-            fle.write('\n{} # Customize python when its launched\n'.format(command))
+            fle.write("\n{} # Customize python when its launched\n".format(command))
         print('Installed in "{}"'.format(customize))
 
 
@@ -90,18 +91,19 @@ def abi_bdist_wheel():
         argument forces the abi tag for wheel builds to this value.
     """
     from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+
     class abi_bdist_wheel(_bdist_wheel):
-        #https://github.com/Yelp/dumb-init/blob/48db0c0d0ecb4598d1a6400710445b85d67616bf/setup.py#L11-L27
+        # https://github.com/Yelp/dumb-init/blob/48db0c0d0ecb4598d1a6400710445b85d67616bf/setup.py#L11-L27
         # Copy user_options from _bdist_wheel and add our argument to the top
         user_options = _bdist_wheel.user_options[:]
-        user_options.insert(0, ('abi=', 'a', 'Custom abi tag, required.'))
+        user_options.insert(0, ("abi=", "a", "Custom abi tag, required."))
 
         def finalize_options(self):
             _bdist_wheel.finalize_options(self)
             # Mark us as not a pure python package
             self.root_is_pure = False
             if self.abi is None:
-                raise ValueError('Parameter --abi must be passed to build this wheel.')
+                raise ValueError("Parameter --abi must be passed to build this wheel.")
 
         def initialize_options(self):
             _bdist_wheel.initialize_options(self)
@@ -110,10 +112,13 @@ def abi_bdist_wheel():
         def get_tag(self):
             python, abi, plat = _bdist_wheel.get_tag(self)
             return python, self.abi, plat
+
     return abi_bdist_wheel
+
 
 def parse_arguments():
     from argparse import ArgumentParser
+
     parser = ArgumentParser(
         description="Forces pip to try one or more custom abi strings before trying "
         "its standard options. This does not require any additional command line "
@@ -121,36 +126,34 @@ def parse_arguments():
         "variable VIRTUALENVCONFIG_VERBOSE to True."
     )
     parser.add_argument(
-        '--install',
-        action='store_true',
-        help='Add/update a sitecustomize.py file to the python site-packages',
+        "--install",
+        action="store_true",
+        help="Add/update a sitecustomize.py file to the python site-packages",
     )
     parser.add_argument(
-        '-o',
-        '--overwrite',
-        action='store_true',
-        help='If sitecustomize.py already exists, update it by adding '
-        '``import virtualenvconfig`` to the end of the file.',
+        "-o",
+        "--overwrite",
+        action="store_true",
+        help="If sitecustomize.py already exists, update it by adding "
+        "``import virtualenvconfig`` to the end of the file.",
     )
     parser.add_argument(
-        '--set-abi',
-        action='append',
-        help='Configure the abi that virtualenvconfig will make pip prefer. '
+        "--set-abi",
+        action="append",
+        help="Configure the abi that virtualenvconfig will make pip prefer. "
         'For example: "vc2010". You can pass this argument more than once to '
-        'add more abi values processed in the order provided. This argument '
-        'always resets the abi settings.',
+        "add more abi values processed in the order provided. This argument "
+        "always resets the abi settings.",
     )
     parser.add_argument(
-        '--clear-abi',
-        action='store_true',
-        help='Remove the abi override.'
+        "--clear-abi", action="store_true", help="Remove the abi override."
     )
     return parser.parse_args()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if not is_venv():
-        print('virtualenvconfig.py should only be run on a active virtualenv.')
+        print("virtualenvconfig.py should only be run on a active virtualenv.")
         sys.exit(1)
 
     args = parse_arguments()
@@ -160,21 +163,21 @@ if __name__ == '__main__':
 
     filename = abi_filename()
     if args.set_abi is not None:
-        with open(filename, 'w') as fle:
+        with open(filename, "w") as fle:
             for abi in args.set_abi:
-                fle.write('{}\n'.format(abi))
-        print('Updated virtualenv abi to :{}'.format(args.set_abi))
+                fle.write("{}\n".format(abi))
+        print("Updated virtualenv abi to :{}".format(args.set_abi))
     if args.clear_abi:
         os.remove(filename)
-        print('Removed virtualenv abi')
+        print("Removed virtualenv abi")
 
 else:
     # Install abi patches for pip
     filename = abi_filename()
     if is_venv() and os.path.exists(filename):
-        abi_overrides = [v.strip() for v in open(filename).read().strip().split('\n')]
+        abi_overrides = [v.strip() for v in open(filename).read().strip().split("\n")]
 
-        logger.debug('Inserting pip abi "{}"'.format(', '.join(abi_overrides)))
+        logger.debug('Inserting pip abi "{}"'.format(", ".join(abi_overrides)))
         import gorilla
         import pip._internal.index
         import pip._internal.pep425tags
@@ -182,39 +185,42 @@ else:
         destinations = [
             # Note: get_supported was removed from index in pip 19.2
             pip._internal.index,
-            pip._internal.pep425tags
+            pip._internal.pep425tags,
         ]
 
         try:
             # target_python was added in pip 19.2
             import pip._internal.models.target_python
+
             destinations.append(pip._internal.models.target_python)
         except ImportError:
             pass
 
         def get_supported(
-            versions=None,
-            noarch=False,
-            platform=None,
-            impl=None,
-            abi=None
+            versions=None, noarch=False, platform=None, impl=None, abi=None
         ):
-            logger.debug('get_supported called')
+            logger.debug("get_supported called")
             # We're overwriting an existing function here,
             # preserve its original behavior.
-            original = gorilla.get_original_attribute(pip._internal.pep425tags, 'get_supported')
-            rows = original(versions=versions, noarch=noarch, platform=platform, impl=impl, abi=abi)
+            original = gorilla.get_original_attribute(
+                pip._internal.pep425tags, "get_supported"
+            )
+            rows = original(
+                versions=versions, noarch=noarch, platform=platform, impl=impl, abi=abi
+            )
 
             # Insert our abi at the top of the stack. preserving the order specified in the file
             for abi_override in reversed(abi_overrides):
                 row = (rows[0][0], abi_override, rows[0][2])
                 rows.insert(0, row)
             for row in rows:
-                logger.debug('  {}'.format(row))
+                logger.debug("  {}".format(row))
 
             return rows
 
         settings = gorilla.Settings(allow_hit=True)
         for destination in destinations:
-            patch = gorilla.Patch(destination, 'get_supported', get_supported, settings=settings)
+            patch = gorilla.Patch(
+                destination, "get_supported", get_supported, settings=settings
+            )
             gorilla.apply(patch)
